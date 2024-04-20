c1dbcda12bb08acf80ed4f7db45aa20163e92299
Philipp Schwarzmueller <philipp.schwarzmueller@gmail.com>
2024-04-20 12:25:14 +0200

    add more token support, page 28

--- token/token.go
+++ token/token.go
@@ -14,8 +14,18 @@ const (
 	IDENT = "IDENT"
 	INT   = "INT"
 
-	ASSIGN    = "="
-	PLUS      = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
+
 	COMMA     = ","
 	SEMICOLON = ";"
 	LPAREN    = "("
@@ -25,4 +35,26 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
