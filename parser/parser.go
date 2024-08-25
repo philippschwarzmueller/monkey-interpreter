1692c2424c04030adb61df28cb54490c72de3056
Philipp Schwarzmueller <philipp.schwarzmueller@gmail.com>
2024-08-25 21:21:25 +0200

    add integer expression parsing

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"pschwarz/interpreter/ast"
 	"pschwarz/interpreter/lexer"
 	"pschwarz/interpreter/token"
+	"strconv"
 )
 
 const (
@@ -42,6 +43,7 @@ func New(l *lexer.Lexer) *Parser {
 	}
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	p.NextToken()
 	p.NextToken()
 	return p
@@ -143,6 +145,21 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	leftExp := prefix()
 
 	return leftExp
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
